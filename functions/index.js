8aa1e816165e47f9109ac9cc71cfe41dbf58853b
Jonathan Goya Nogiri <jonathannogiri@gmail.com>
2023-02-19 17:23:08 +0000

    Adjusting Workers

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,17 @@
 
-const form = document.querySelector('form')
-const searchInput = document.querySelector('#search')
-const resultsDiv = document.querySelector('#results')
 
-form.addEventListener('submit', async event => {
-  event.preventDefault()
-  const searchQuery = searchInput.value
-  const url = `https://logparse.jonathannogiri1211.workers.dev/?q=${searchQuery}`
-  const response = await fetch(url)
-  const results = await response.json()
-  resultsDiv.innerHTML = results.map(result => `<p>${result}</p>`).join('')
-})
+const searchLogs = async (searchParams) => {
+    const workerUrl = 'https://logparse.jonathannogiri1211.workers.dev/';
+    const response = await fetch(`${workerUrl}?search=${searchParams}`);
+    const result = await response.json();
+    return result;
+  };
+  
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const searchParams = formData.get('search');
+    const searchResult = await searchLogs(searchParams);
+    // Display search result on page
+    console.log(searchResult);
+  };
